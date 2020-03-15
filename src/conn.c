1e9f32e208043ba45fc35f26cf633ac1916c0cb9
Zhuoheng Li <zhuoheng@Zhuohengs-MacBook-Pro.local>
2020-03-15 19:58:35 -0400

    add conn.c

---
+++ src/conn.c
@@ -0,0 +1,25 @@
+/* Sort the recevied packets array */
+void sort_packets(packet* packet_array){
+
+}
+
+/* Split the raw data input to multiple slices */
+void split_data(char* raw_data) {
+
+}
+
+
+/* Reorganize the sorted packets array */
+void reorg(packet* packet_array){
+
+}
+
+//  Use passive probing to test the condition of the path 
+// double probe(pathInfo* path_array);
+
+// implement sender function
+
+// implement receiver function
+
+
+
